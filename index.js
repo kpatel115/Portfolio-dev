9ba56e349220fb41ba7e00a5deadc3f6abfeb7f1
kpatel115 <kpatel9338448@gmail.com>
2024-05-13 08:52:39 -0600

    updated portfolio to more advanced projects

---
+++ index.js
@@ -0,0 +1,19 @@
+let players1 = []
+let players2 = []
+let team1 = {
+    name: "Bayern Munich",
+    gk: "", // goalkeeper 1
+    fieldPlayers:[
+        // 10 players
+    ]
+}
+let allPlayers = []
+let playersFinal = [] // original 10 + 3 substitutes
+
+let game = {
+    odds: ['team1', 'draw', 'team2']
+}
+
+const printGoals = (numPlayers) => {
+    console.log()
+}
